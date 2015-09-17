c0facb140cacdfdafa944aaabeb7713ed696b0dd
Etore Marcari Junior <etore@dpi.inpe.br>
2015-09-17 14:27:12 -0300

    Ticket #10: removendo metodos nao usados e adicionando testes do radarFunctions

--- tests/unittest/common/radarFunctions_unitTest.cpp
+++ tests/unittest/common/radarFunctions_unitTest.cpp
@@ -12,13 +12,84 @@ TerraRadar is under development.
 
 // TerraRadar includes
 #include "BuildConfig.hpp"
+#include "Functions.hpp"
+#include "RadarFunctions.hpp"
+#include "Utils.hpp"
+
+// TerraLib includes
+#include <terralib/common/TerraLib.h>
+#include <terralib/plugin.h>
 
 // Gtest includes
 #include <gtest/gtest.h>
 
-TEST(RadarFunctions, myMocTest)
+TEST( InitMethods, loadTerralib )
 {
-	EXPECT_EQ(1000, 1000);
+  // load TerraLib and it drivers
+  TerraLib::getInstance().initialize();
+  teradar::common::loadTerraLibDrivers();
+}
 
+TEST( RadarFunctions, createCovarianceTest1 )
+{
+  // open the input raster
+  std::map<std::string, std::string> inputRasterInfo;
+  inputRasterInfo["URI"] = TERRARADAR_DATA_DIR "/rasters/ref_ImagPol240_0.bin";
+  te::rst::Raster* inputRaster( te::rst::RasterFactory::open( inputRasterInfo ) );
+  EXPECT_TRUE( inputRaster != NULL );
 
+  // create input data (bands and rasters)
+  std::vector<unsigned int> inputBands;
+  std::vector<te::rst::Raster*> inputRasters;
+
+  for( unsigned int i = 0; i < 3; ++i ) {
+    inputBands.push_back( i );
+    inputRasters.push_back( inputRaster );
+  }
+
+  // call CreateCovarianceRaster and check the return value.
+  std::auto_ptr<te::rst::Raster> covMatrixRaster;
+  std::map<std::string, std::string> covRasterInfo;
+
+  bool covResult = teradar::common::CreateCovarianceRaster( inputRasters, inputBands, covRasterInfo, "MEM", covMatrixRaster );
+  EXPECT_TRUE( covResult );
+
+  // save the created file
+  teradar::common::CopyComplex2DiskRaster( *covMatrixRaster, "test_ImagPol240_0_cov.tif" );
+
+  // @todo - etore - check if the image has been generated correctly.
+
+  // Free memory
+  delete inputRaster;
+}
+
+TEST( RadarFunctions, createIntensityTest1 )
+{
+  // open the input raster
+  std::map<std::string, std::string> inputRasterInfo;
+  inputRasterInfo["URI"] = TERRARADAR_DATA_DIR "/rasters/ref_ImagPol240_0_cov.tif";
+  te::rst::Raster* inputRaster( te::rst::RasterFactory::open( inputRasterInfo ) );
+  EXPECT_TRUE( inputRaster != NULL );
+
+  // call CreateIntensityRaster and check the return value.
+  std::auto_ptr<te::rst::Raster> intensityRaster;
+  std::map<std::string, std::string> intRasterInfo;
+
+  bool intResult = teradar::common::CreateIntensityRaster( inputRaster, intRasterInfo, "MEM", intensityRaster );
+  EXPECT_TRUE( intResult );
+
+  // save the created file
+  teradar::common::CopyComplex2DiskRaster( *intensityRaster, "test_ImagPol240_0_int.tif" );
+
+  // @todo - etore - check if the image has been generated correctly.
+
+  // Free memory
+  delete inputRaster;
+}
+
+TEST( EndMethods, finalizeTerralib )
+{
+  te::plugin::PluginManager::getInstance().shutdownAll();
+  te::plugin::PluginManager::getInstance().unloadAll();
+  TerraLib::getInstance().finalize();
 }
